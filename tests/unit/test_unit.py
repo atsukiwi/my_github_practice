4c495a3071e00428a4f1d6bf834b60847c9de7d5
atsukiwi <atsuki.hattori@daedalus.co.jp>
2022-08-02 15:01:16 +0900

    check

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -7,7 +7,7 @@ def test_unit_hoge(a):
     a_result = task0.func_hoge(a)
     assert a_expected == a_result
 
-@pytest.mark.parametrize("a",["a", 1])
+@pytest.mark.parametrize("a",["a", "b"])
 def test_unit_fuga(a):
     a_expected = str(a) + "_OUTPUT"
     a_result = task0.func_fuga(a)
