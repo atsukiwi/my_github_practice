018b49eb13a874ff12ea56c7c36a16d2ee22c718
atsukiwi <atsuki.hattori@daedalus.co.jp>
2023-03-15 17:58:08 +0900

    comit

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from importlib_metadata import version
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name="tasks",
     version="0.1.0",
-
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
 )
